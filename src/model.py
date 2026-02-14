Fixed NaN issue with actor network by normalizing RSRP calculation.

src/model.py
@@ -9,44 +9,44 @@ class Critic(torch.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         hidden_size = 16
-        self.conv1 = torch_geometric.nn.GCNConv(4, hidden_size)
-        self.conv2 = torch_geometric.nn.GCNConv(hidden_size,hidden_size)
-        self.conv3 = torch_geometric.nn.GCNConv(hidden_size, 1)
+        self.convlayers: list[torch_geometric.nn.GCNConv] = [
+            torch_geometric.nn.GCNConv(-1,hidden_size),
+            torch_geometric.nn.GCNConv(hidden_size,hidden_size),
+            torch_geometric.nn.GCNConv(hidden_size, hidden_size),
+            torch_geometric.nn.GCNConv(hidden_size, 1)
+        ]
 
-    def forward(self, edges: torch.Tensor, weights: torch.Tensor, vertex_features: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(vertex_features, edges, weights)
-        x = torch.relu(x)
+        self.lin = torch_geometric.nn.Linear(hidden_size,1)
 
-        x = self.conv2(vertex_features, edges, weights)
-        x = torch.relu(x)
+    def forward(self, vertex_features: torch.Tensor, edges: torch.Tensor, weights: torch.Tensor, ) -> torch.Tensor:
+        for layer in self.convlayers:
+            vertex_features = layer(x=vertex_features, edge_index=edges, edge_weight=weights)
+            vertex_features = torch.relu(vertex_features)
 
-        x = self.conv3(vertex_features, edges, weights)
-        x = torch.relu(x)
-
-        q_value = torch_geometric.nn.global_mean_pool.global_mean_pool(x, torch.tensor(len(vertex_features)))
-
-        return q_value
+        node_emb = torch_geometric.nn.pool.global_mean_pool(x=vertex_features, batch=None)
+        return node_emb
 
 class Actor(torch.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        hidden_size = 16
-        self.conv1 = torch_geometric.nn.GCNConv(4, hidden_size)
-        self.conv2 = torch_geometric.nn.GCNConv(hidden_size,hidden_size)
-        self.conv3 = torch_geometric.nn.GCNConv(hidden_size, 1)
+        hidden_size = 64
+        self.convlayers: list[torch_geometric.nn.GCNConv] = [
+            torch_geometric.nn.GCNConv(6,hidden_size),
+            torch_geometric.nn.GCNConv(hidden_size,hidden_size),
+            torch_geometric.nn.GCNConv(hidden_size, hidden_size)
+        ]
 
-    def forward(self, edges: torch.Tensor, weights: torch.Tensor, vertex_features: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(vertex_features, edges, weights)
-        x = torch.relu(x)
+        self.lin = torch_geometric.nn.Linear(hidden_size,1)
 
-        x = self.conv2(vertex_features, edges, weights)
-        x = torch.relu(x)
+    def forward(self, vertex_features: torch.Tensor, edges: torch.Tensor, weights: torch.Tensor, ) -> torch.Tensor:
+        torch.autograd.set_detect_anomaly(True)
+        for layer in self.convlayers:
+            vertex_features = layer(x=vertex_features, edge_index=edges, edge_weight=weights)
+            vertex_features = torch.sigmoid(vertex_features)
 
-        x = self.conv3(vertex_features, edges, weights)
-        x = torch.relu(x)
+        node_emb: torch.Tensor = self.lin(vertex_features)
+        return torch.softmax(node_emb, 0)
 
-        return x
-    
 class A2CAgent(torch.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)

src/nr.py
@@ -34,9 +34,14 @@ class ProportionalFairScheduler:
         self.total_prbs = total_prbs
 
     def calculate_metric(self, ue: 'NrUe') -> float:
-        if ue.average_throughput <= 0:
-            return np.inf
+        """
+        Calculate PF metric for the given UE. 
         
+        :param ue: The UE to calculate the PF metric for.
+        :type ue: 'NrUe'
+        :return: The PF metric
+        :rtype: float
+        """
         return ue.instantaneous_rate / ue.average_throughput
     
     def schedule(self, ues: list['NrUe']) -> dict['NrUe', int]:
@@ -45,7 +50,9 @@ class ProportionalFairScheduler:
             if ue.instantaneous_rate > 0
         ]
 
-        if not active_users:
+        #print(active_users)
+
+        if len(active_users) < 1:
             return {}
         
         user_metrics = []
@@ -244,7 +251,7 @@ class NrUe:
         self.id: int = id
 
         self.average_throughput: float = 1.0
-        self.instantaneous_rate: float = 0
+        self.instantaneous_rate: float = 10e-6 # small number to prevent division by zero error
 
         self.assigned_prbs_history: list[float] = []
         self.throughput_history: list[float] = []
@@ -266,6 +273,7 @@ class NrUe:
 
     def update_instantaneous_rate(self) -> None:
         if not self.serving_gnb:
+            # print("No serving gnb")
             self.instantaneous_rate = -1
             return
         
@@ -283,7 +291,7 @@ class NrUe:
         bits_per_re = se  # bits per resource element
         re_per_prb = 12 * 14
         rate_per_prb = re_per_prb * bits_per_re * 1000  # per second
-
+        # print(rate_per_prb)
         self.instantaneous_rate = rate_per_prb
 
     def update_average_throughput(self, actual_rate: float, alpha: float, 

src/simulation.py
@@ -144,24 +144,30 @@ class Simulation:
         """
         n = len(self.ues)
         m = len(self.gnbs)
-        e: list[tuple[int,int]] = []
-        w: list[float] = [-np.inf for _ in range(m + n)]
-        v: list[list[float]] = [[0,0,0,0] for _ in range(m + n)]
+        source: list [int] = []
+        destination: list [int] = []
+        w: list[float] = []
+        v: list[list[float]] = [[0,0,0,0,0,0] for _ in range(m + n)]
 
         for gnb in self.gnbs.values():
-            v[gnb.cell_id] = [gnb.position.x, gnb.position.y, gnb.position.z, gnb.radio_unit.advanced_sleep_mode.value[2], gnb.ue_scheduler.total_prbs / sum(prb for prb in gnb.allocate().values()), sum(ue.average_throughput for ue in gnb.connected_ues) / len(gnb.connected_ues)]
+            prb_utilization = sum(prb for prb in gnb.allocate().values()) / gnb.ue_scheduler.total_prbs
+            average_throughput = 1
+            if len(gnb.connected_ues) > 0:
+                average_throughput = sum(ue.average_throughput for ue in gnb.connected_ues) / len(gnb.connected_ues)
+            v[gnb.cell_id] = [gnb.position.x, gnb.position.y, gnb.position.z, gnb.radio_unit.advanced_sleep_mode.value[2], prb_utilization, average_throughput]
             for ue in gnb.connected_ues:
-                e.append((gnb.cell_id, ue.id))
-                w[ue.id] = rsrp(gnb.position, ue.position, gnb.tx_freq, gnb.tx_power)
+                source.append(gnb.cell_id)
+                destination.append(ue.id)
+                w.append(rsrp(gnb.position, ue.position, gnb.tx_freq, gnb.tx_power)/-160)
 
         for ue in self.ues.values():
-            v[ue.id] = [ue.position.x, ue.position.y, ue.position.z, -1, -1, -1]
+            v[m + ue.id] = [ue.position.x, ue.position.y, ue.position.z, 1, 1, 1]
 
-        feature_vector: torch.Tensor = torch.tensor(v)
-        edge_vector: torch.Tensor = torch.Tensor(e)
-        weight_vector: torch.Tensor = torch.Tensor(w)
+        feature_vector: torch.Tensor = torch.tensor(v, dtype=torch.float32)
+        edge_vector: torch.Tensor = torch.tensor([source, destination], dtype=torch.int)
+        weight_vector: torch.Tensor = torch.tensor(w, dtype=torch.float32)
 
-        return feature_vector, edge_vector, weight_vector
+        return feature_vector, edge_vector.to(torch.int64), weight_vector
 
     def schedule_event(self, time_to_execute: float, action: ActionType, args: dict | None = None) -> None:
         self.queue.append(Event(self.time + time_to_execute, action, self, args))
@@ -192,16 +198,18 @@ class Simulation:
             self.gnbs[i] = NrGnb(0,0,25,25,i,self)
             self.gnbs[i].parent_scheduler = self
 
-            self.items.append(self.gnbs[i])
-
         for i in range(m):
             self.ues[i] = NrUe(0,0,1.5,0, 1.5, i, self)
             self.ues[i].parent_scheduler = self
 
+            if np.random.randint(0,2) == 0:
+                self.ues[i].velocity.x *= -1
+            
+            if np.random.randint(0,2) == 0:
+                self.ues[i].velocity.y *= -1
+
             self.ues[i].position.x = np.random.randint(-350, 350)
             self.ues[i].position.y = np.random.randint(-350, 350)
-
-            self.items.append(self.ues[i])
 
         self.gnbs[0].position = Vector(0,0,25)
 
@@ -283,11 +291,12 @@ class Simulation:
                     best_gnb.connected_ues.append(ue)
                     if self.energy_saving_strategy == GnbSleepModeStrategy.Naive and best_gnb.radio_unit.advanced_sleep_mode != AdvancedSleepMode.ACTIVE:
                         self.set_advanced_sleep_mode(best_gnb, AdvancedSleepMode.ACTIVE)
-                    else:
-                        best_gnb.allocate()
-                    
-
                     print("Attach UE with ID ", ue.id, "to gNB with ID ", best_gnb.cell_id)
+
+        # Allocate PRBs for attached UEs
+        for gnb in self.gnbs.values():
+            gnb.allocate()
+            # print(alloc)
                         
 
         # Change UE position based on mobility model
@@ -297,15 +306,8 @@ class Simulation:
                 new_position_dx = ue.velocity.x * self.delta
                 new_position_dy = ue.velocity.y * self.delta
                 
-                if np.random.randint(0,2) == 0:
-                    new_position.x = ue.position.x + new_position_dx
-                else:
-                    new_position.x = ue.position.x - new_position_dx
-
-                if np.random.randint(0,2) == 0:
-                    new_position.y = ue.position.y + new_position_dy
-                else:
-                    new_position.y = ue.position.y - new_position_dy
+                new_position.x = ue.position.x + new_position_dx
+                new_position.y = ue.position.y + new_position_dy
 
                 ue.set_position(new_position)
 